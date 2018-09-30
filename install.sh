39ba584185991ab2cd09dfbbb6ed25e561046660
Yubin Lee <jordanyubin@naver.com>
2018-09-30 20:13:02 +0300

    Shebang

--- install.sh
+++ install.sh
@@ -1,15 +1,10 @@
+#!/bin/sh
+
 sudo apt-get install gawk bison flex libbluetooth-dev libgtk2.0-dev libgtk2.0 autoconf autogen automake bluetooth pkg-config python-dev git
-
 git clone https://github.com/azzra/python3-wiimote.git
-
 cd python3-wiimote
-
 sudo aclocal
-
 sudo autoconf
-
 sudo ./configure
-
 sudo make
-
 sudo make install
